cc91b0798132c6ccf5fc2e5bfecccb3f0907aa37
Seoldongmin <125715732+Seoldongmin@users.noreply.github.com>
2023-04-23 22:13:26 +0900

    Add files via upload

---
+++ 20230423/20230423_28/main.cpp
@@ -0,0 +1,25 @@
+// 파이 구하기 299pg
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	double divisor, divident, sum;
+	int loop_count;
+
+	divisor = 1.0;
+	divident = 4.0;
+	sum = 0.0;
+	printf("반복횟수:");
+	scanf("%d", &loop_count);
+
+	while (loop_count > 0)
+	{
+		sum = sum + divident / divisor;
+		divident = -1.0 * divident;
+		divisor = divisor + 2;
+		loop_count--;
+	}
+
+	printf("파이값 = %lf\n", sum);
+}
