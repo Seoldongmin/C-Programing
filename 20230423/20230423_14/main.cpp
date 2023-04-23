971bb424f3cfbdc5678540d0cd075b55935478f9
Seoldongmin <125715732+Seoldongmin@users.noreply.github.com>
2023-04-23 22:08:20 +0900

    Add files via upload

---
+++ 20230423/20230423_14/main.cpp
@@ -0,0 +1,9 @@
+// "Hello Worid! 5번 출력하기
+#include <stdio.h>
+
+void main()
+{
+	int i;
+	for (i = 0; i < 5; i++)                  // i 는 0부터 4까지 증가
+		printf("Hello Worid! \n");
+}
---
+++ 20230423/20230423_15/main.cpp
@@ -0,0 +1,12 @@
+//  반복을 이용한 정수합 프로그램
+#include <stdio.h>
+
+void main()
+{
+	int sum = 0;
+
+	for (int i = 1; i <= 10; i++)
+		sum += i;
+
+	printf("1부터 10까지의 정수의 합= %d\n", sum);
+}
