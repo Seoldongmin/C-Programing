703bfc75172e30c28dfe4634b1576321b6fe1765
Seoldongmin <125715732+Seoldongmin@users.noreply.github.com>
2023-04-24 20:24:41 +0900

    Add files via upload

---
+++ 20230422/20230422_2/main.cpp
@@ -0,0 +1,19 @@
+// 10¹ø
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()    
+{
+    int i, j;
+
+    for (j = 2; j < 100; j++)
+    {
+        for (i = 2; i < j; i++)
+        {
+            if (j % i == 0)
+                break;
+        }
+        if (i == j)
+            printf("%d ", i);
+    }
+}
